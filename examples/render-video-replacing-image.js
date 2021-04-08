3df7860078a341686a5b4f801348dab5612530bc
Albin Larsson <mail@albinlarsson.com>
2021-04-08 15:23:53 +0200

    Add more options and include the pie image

--- examples/render-video-replacing-image.js
+++ examples/render-video-replacing-image.js
@@ -5,7 +5,12 @@ const renderAnimation = require("./render-animation");
   const data = JSON.parse((await readFile("18162-rotating-sun.json")).toString());
 
   // Replace image of the sun with local image of a pie
-  data.assets[0].p = 'photo-1572383672419-ab35444a6934.png'
+  // You only need to set `p` to the image src for the example to work, but the other options may be needed for other data files
+  Object.assign(data.assets[0], {
+    p: 'photo-1572383672419-ab35444a6934.png', // p is the image src
+    e:  1, // Ignore image folder path. You can also set data.assets[0].u to an empty string
+    pr: 'xMidYMid meet' // You may want to "contain" the image, see https://github.com/airbnb/lottie-web/issues/1046  
+  });
 
   await renderAnimation({
     data,
